15fee240e2ad4a9cbbd341881f309e3049ec8266
Luasza <148979226+Luasza@users.noreply.github.com>
2024-02-14 21:27:26 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const nome = 'Luana'
+const sobrenome = 'Souza'
+const anoAtual = 2024
+const idade = 29
+
+
+console.log(`${nome} ${sobrenome}`)
+console.log(anoAtual - idade)
+
+
+
+let idadeInserida = prompt ('Insira a sua idade')
+
+alert (' A idade inserida foi: ' + idadeInserida)
